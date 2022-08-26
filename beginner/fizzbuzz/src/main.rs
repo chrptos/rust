8f53ff1bd438ceb7e206ddb520bf369e42fc14d1
chrptos <chrptos@gmail.com>
2022-08-26 19:08:44 +0900

    test実装

--- beginner/fizzbuzz/src/main.rs
+++ beginner/fizzbuzz/src/main.rs
@@ -1,18 +1,18 @@
-// fn fizzbuzz(end: i32) {
-//     let mut x: i32 = 1;
-//     while x <= end {
-//         if x % 3 == 0 && x % 5 == 0 {
-//             println!("FizzBuzz");
-//         } else if x % 3 == 0 {
-//             println!("Fizz");
-//         } else if x % 5 == 0 {
-//             println!("Buzz");
-//         } else {
-//             println!("{}", x);
-//         }
-//         x += 1;
-//     }
-// }
+fn fizzbuzz(end: i32) {
+    let mut x: i32 = 1;
+    while x <= end {
+        if x % 3 == 0 && x % 5 == 0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }
+        x += 1;
+    }
+}
 
 // fn fizzbuzz(end: i32) {
 //     let mut r: = 1..=end;
@@ -26,18 +26,16 @@
 //     }
 // }
 
-fn fizzbuzz(end: i32) {
-    let r: RangeInclusive<i32> = 1..=end;
-
-    for x: i32 in r {
-        match (x % 3, x % 5) {
-            (0, 0) => println!("FizzBuzz"),
-            (0, _) => println!("Fizz"),
-            (_, 0) => println!("Buzz"),
-            _ => println!("{}", x),
-        }
-    }
-}
+// fn fizzbuzz(end: i32) {
+//     for x: i32 in 1..=end {
+//         match (x % 3, x % 5) {
+//             (0, 0) => println!("FizzBuzz"),
+//             (0, _) => println!("Fizz"),
+//             (_, 0) => println!("Buzz"),
+//             _ => println!("{}", x),
+//         }
+//     }
+// }
 
 fn main() {
     fizzbuzz(30);
